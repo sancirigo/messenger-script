ec065645b7775a1fe660bec9180280dcad6faea4
sancirigo <sancirigo@gmail.com>
2023-02-05 22:16:47 +0100

    pettier

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pyautogui
 import keyboard
 
 
-def write(text: str) -> None:
-    pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('home.png')))
-    pyautogui.typewrite(text + "\n")
+def write(img: str, text: str) -> None:
+    if (pyautogui.locateOnScreen(img) is not None):
+        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('home.png')))
+        pyautogui.typewrite(text + "\n")
 
 
 def main():
@@ -12,11 +13,9 @@ def main():
         if (keyboard.is_pressed('F10')):
             exit(0)
 
-        if (pyautogui.locateOnScreen("jolvagy.png") is not None):
-            write("Igen igen")
+        write("jolvagy.png", "Igen igen")
 
-        if (pyautogui.locateOnScreen("hogyvagy.png") is not None):
-            write("Jol")
+        write("hogyvagy.png", "Jol")
 
 
 if __name__ == "__main__":
